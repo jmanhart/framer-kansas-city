4764952c12d567219510c392944a76d8405ade07
jmanhart <manhartjohn@gmail.com>
2019-01-30 14:52:54 -0600

    designing out some screens for a fake podcast app in framer x

--- framer-x/playground/001-playground.framerfx/code/canvas.tsx
+++ framer-x/playground/001-playground.framerfx/code/canvas.tsx
@@ -1,4 +1,4 @@
 // WARNING: this file is auto generated, any changes will be lost
 import { createDesignComponent, CanvasStore } from "framer"
 const canvas = CanvasStore.shared(); // CANVAS_DATA;
-export const ButtonBlock = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_j8EFZYbYr", {}, 375,60);
+export const Component = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_NDyfxaW2B", {}, 64,67);
